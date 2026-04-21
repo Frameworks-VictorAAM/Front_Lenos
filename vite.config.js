957a9d0123a1a6724f1f2ceb42f99d0c7d2f0ae0
Victor Aldavera <1224100623.vaam@gmail.com>
2026-04-20 21:29:31 -0600

    orale

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Asegura que las rutas de los assets empiecen desde la raíz
+})
+
+
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
